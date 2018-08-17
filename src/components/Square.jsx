8b065d3cd82c52007767fcc15eae9e723d6b58f0
ghost-fk <fayyad_r22@yahoo.com>
2018-08-17 16:10:28 +0200

    add a constructor to the class to initialize the state

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
   render() {
     return (
       <button className="square" onClick={() => alert('click')}>

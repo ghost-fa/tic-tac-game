c15674e7f6a40c1b90b14bf92cc5fcb1658aa305
ghost-fk <fayyad_r22@yahoo.com>
2018-08-17 16:07:01 +0200

    add onclick to the squery component when we click will open alert with click

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class Square extends Component {
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => alert('click')}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 export default Square;

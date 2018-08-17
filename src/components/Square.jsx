e85db60923a61a93d343e089d725ff3231f8474e
ghost-fk <fayyad_r22@yahoo.com>
2018-08-17 17:30:40 +0200

    i replace the square class with the function

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React  from 'react';
 
-class Square extends Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
+const Square =(props) {
+  return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
-  }
-}
+
+};
 export default Square;
